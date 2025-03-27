da0f860fcc25cac2344e4d7fcf666dcf0df05f47
AryanAnujitSinha <aryananujit@gmail.com>
2025-03-27 14:44:48 +0530

    Update project.js

--- project.js
+++ project.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const sensorDataDiv = document.getElementById("sensorData");
 
   setInterval(() => {
-    const temperature = (Math.random() * 5 + 25).toFixed(1);
-    const humidity = (Math.random() * 11 + 40 ).toFixed(1);
-    const nutrientLevel = (Math.random() * 2 + 5.5).toFixed(1);
+    const temperature = (Math.random() * 1 + 26).toFixed(1);
+    const humidity = (Math.random() * 5 + 50 ).toFixed(1);
+    const nutrientLevel = (Math.random() * 1 + 5.5).toFixed(1);
 
     sensorDataDiv.innerHTML = `
       <p>Temperature: ${temperature} °C</p>
